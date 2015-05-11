a989de76bc082b506450d2ae1a4d8861a9b99436
Chiel Kunkels <chiel@kunkels.me>
2015-05-11 21:51:01 +0200

    reformat docs to jsdoc

--- eve.js
+++ eve.js
@@ -7,7 +7,7 @@ var argv = require('yargs').default('c', './config.json').argv;
 var path = require('path');
 var config = require(path.resolve(argv.c));
 
-var eve = require('./lib/eve')(config);
+var eve = new (require('./lib/eve'))(config);
 
 eve.on('connected', function(){
 	eve.loadModules(path.resolve('modules'));
